231209cb917cd18ec3cc97730e22e820d22e9aa5
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-07-03 14:44:43 +0200

    fix: enable powers of negative numbers

--- src/mammos_analysis/kuzmin.py
+++ src/mammos_analysis/kuzmin.py
@@ -182,10 +182,13 @@ def kuzmin_formula(Ms_0, T_c, s, T):
     if isinstance(T, me.Entity):
         T = T.value
     base = 1 - s * (T / T_c) ** 1.5 - (1 - s) * (T / T_c) ** 2.5
-    out = np.zeros_like(T, dtype=np.float64)
-    # only compute base**(1/3) where T < T_c; elsewhere leave as zero
-    np.power(base, 1 / 3, out=out, where=(T_c > T))
-    return Ms_0 * out
+    base = np.array(base)  # we make sure that it is a numpy.ndarray
+    pos = np.zeros_like(base, dtype=np.float64)
+    neg = np.zeros_like(base, dtype=np.float64)
+    np.power(base, 1 / 3, out=pos, where=np.logical_and(T_c > T, base > 0))
+    np.power(-base, 1 / 3, out=neg, where=np.logical_and(T_c > T, base < 0))
+    # both pos and neg will be zero when T >= T_c
+    return Ms_0 * (pos - neg)
 
 
 class _A_function_of_temperature:
